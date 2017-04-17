0bbe9b97d8d3f07cfc761cf8d5b2c9ae59f05bc4
kenzhong <kenzhong@quanyibao.com>
2017-04-17 17:25:20 +0800

    save

--- service0/src/main/java/cn/demo/service0/config/AmqpConfig.java
+++ service0/src/main/java/cn/demo/service0/config/AmqpConfig.java
@@ -1,41 +1,52 @@
 package cn.demo.service0.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
 //@Configuration
 public class AmqpConfig {
-//    @Bean
-//    RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-//        return new RabbitAdmin(connectionFactory);
-//    }
-//
-//    @Bean
-//    public RabbitMessagingTemplate rabbitMessagingTemplate(RabbitTemplate rabbitTemplate) {
-//        RabbitMessagingTemplate rabbitMessagingTemplate = new RabbitMessagingTemplate();
-//        rabbitMessagingTemplate.setMessageConverter(new MappingJackson2MessageConverter());
-//        rabbitMessagingTemplate.setRabbitTemplate(rabbitTemplate);
-//        return rabbitMessagingTemplate;
-//    }
-//
-//    @Bean
-//    DirectExchange contractDirectExchange(RabbitAdmin rabbitAdmin) {
-//        DirectExchange contractDirectExchange = new DirectExchange(RabbitmqExchange.TRANSACTION_EXCHANGE);
-//        rabbitAdmin.declareExchange(contractDirectExchange);
-//        return contractDirectExchange;
-//    }
-//
-//    @Bean
-//    Binding bindingExchangeContract(Queue queueContract, DirectExchange exchange, RabbitAdmin rabbitAdmin) {
-//        Binding binding = BindingBuilder.bind(queueContract).to(exchange).with(RabbitmqQueue.TRANSACTION_QUEUE);
-//        rabbitAdmin.declareBinding(binding);
-//        return binding;
-//    }
-//
-//    @Bean
-//    Queue queueContract(RabbitAdmin rabbitAdmin) {
-//        Queue queue = new Queue(RabbitmqQueue.TRANSACTION_QUEUE, true);
-//        rabbitAdmin.declareQueue(queue);
-//        return queue;
-//    }
-//
+    @Bean
+    RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public RabbitMessagingTemplate rabbitMessagingTemplate(RabbitTemplate rabbitTemplate) {
+        RabbitMessagingTemplate rabbitMessagingTemplate = new RabbitMessagingTemplate();
+        rabbitMessagingTemplate.setMessageConverter(new MappingJackson2MessageConverter());
+        rabbitMessagingTemplate.setRabbitTemplate(rabbitTemplate);
+        return rabbitMessagingTemplate;
+    }
+
+    @Bean
+    DirectExchange contractDirectExchange(RabbitAdmin rabbitAdmin) {
+        DirectExchange contractDirectExchange = new DirectExchange(RabbitmqExchange.TRANSACTION_EXCHANGE);
+        rabbitAdmin.declareExchange(contractDirectExchange);
+        return contractDirectExchange;
+    }
+
+    @Bean
+    Binding bindingExchangeContract(Queue queueContract, DirectExchange exchange, RabbitAdmin rabbitAdmin) {
+        Binding binding = BindingBuilder.bind(queueContract).to(exchange).with(RabbitmqQueue.TRANSACTION_QUEUE);
+        rabbitAdmin.declareBinding(binding);
+        return binding;
+    }
+
+    @Bean
+    Queue queueContract(RabbitAdmin rabbitAdmin) {
+        Queue queue = new Queue(RabbitmqQueue.TRANSACTION_QUEUE, true);
+        rabbitAdmin.declareQueue(queue);
+        return queue;
+    }
+
     public interface RabbitmqExchange {
         String TRANSACTION_EXCHANGE = "Transaction_Exchange";
     }
--- service0/src/main/java/cn/demo/service0/config/ConsumerConfig.java
+++ service0/src/main/java/cn/demo/service0/config/ConsumerConfig.java
@@ -1,23 +1,30 @@
 package cn.demo.service0.config;
 
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+
 //@Configuration
 //@EnableRabbit
-public class ConsumerConfig{
-//    implements
-//} RabbitListenerConfigurer {
-//
-//    @Override
-//    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
-//        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
-//        factory.setMessageConverter(new MappingJackson2MessageConverter());
-//        registrar.setMessageHandlerMethodFactory(factory);
-//    }
-//
-//    @Bean
-//    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-//        factory.setConnectionFactory(connectionFactory);
-//        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
-//        return factory;
-//    }
+public class ConsumerConfig implements RabbitListenerConfigurer {
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(new MappingJackson2MessageConverter());
+        registrar.setMessageHandlerMethodFactory(factory);
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        return factory;
+    }
 }
